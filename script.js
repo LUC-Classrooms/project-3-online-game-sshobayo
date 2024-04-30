617a9e43725f2c92947c0375a227295795e40651
sshobayo <157627962+sshobayo@users.noreply.github.com>
2024-04-30 00:06:01 +0000

    Version 1 - player1 movemen

--- script.js
+++ script.js
@@ -4,22 +4,16 @@
  * 
  * Use this template to get started creating a simple 2D game for the web using P5.js. 
  */
+var gameState = "splash";
+var player1;
 
 function setup() {
- var gameState = "splash";
-  createCanvas(600, 400);
 
-  // When mouse is clicked
-function mousePressed() {
-  // the current game transitions
-  if (gameState == "splash") {
-    gameState = "play"; // Move to "play" screen
-  } else if (gameState == "play") {
-    gameState = "gameOver"; // Move to "gameOver" screen
-  } else if (gameState == "gameOver") {
-    gameState = "splash"; // Move back to "splash" screen
-  }
-}
+  createCanvas(600, 400);
+player1 = new Player(width/2, height * 7/8);
+console.log(player1);
+
+
 }
 
 function draw() {
@@ -28,6 +22,19 @@ function draw() {
   //splash(); // call the splash screen function (below)
   //play(); // call the play screen function (below)
   //gameOver(); // call the gameOver screen function (below)
+switch (gameState) {
+  case "splash" :
+    splash();
+    break;
+  case "play" :
+    play();
+    break;
+  case "gameOver" :
+    gameOver();
+    break;
+  default :
+    console.log("no match! check your mousePressed function")
+}
 
 }
 
@@ -48,7 +55,10 @@ function play() // draw the "play" screen {
   textAlign(CENTER);
   textSize(16);
   text("This is where the Game happens", width / 2, height / 2);
-
+  player1.x = mouseX;
+  player1.y = mouseY;
+  player1.display();
+  
 }
 
 function gameOver()// Draw the "gameOver" screen {
@@ -61,6 +71,14 @@ function gameOver()// Draw the "gameOver" screen {
 }
 
 function mousePressed() {
+
+    if (gameState == "splash") {
+      gameState = "play"; //go to the play() screen
+    }else if (gameSate == "play") {
+      gameState = "gameOver";
+    }else if (gameSate == "gameOver"){
+      gameState = "splash";
+    }
 
   console.log("click!");
 
