ca0783589256049cb221d2cec62ab78aaea885cd
David B Wetzel <david@familycatproductions.com>
2024-05-01 13:36:09 +0000

    Error corrections

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
  * Project 3 versions 0-4 - 2D Web Game
  * Name:splash
  * 
+ * Update May 1, 2024 - I made some corrections, especially where a lot of the game code was sitting outside the draw() function. - Dr. Wetzel
+ * 
  * Use this template to get started creating a simple 2D game for the web using P5.js. 
  */
 var gameState = "splash"; // lab 13
@@ -21,12 +23,9 @@ function setup() {
   console.log(player1);
 
   timer = new Timer (30000); // 30 seconds
-  dropTImer = new TImer(1000); // 1 second
-
+  dropTimer = new Timer(1000); // 1 second
 
   console.log(timer);
-
-
 }
 
 function draw() {
@@ -35,19 +34,19 @@ function draw() {
   //splash(); // call the splash screen function (below)
   //play(); // call the play screen function (below)
   //gameOver(); // call the gameOver screen function (below)
-switch (gameState) {
-  case "splash" :
-    splash();
-    break;
-  case "play" :
-    play();
-    break;
-  case "gameOver" :
-    gameOver();
-    break;
-  default :
-    console.log("no match! check your mousePressed function")
-}
+  switch (gameState) {
+    case "splash" :
+      splash();
+      break;
+    case "play" :
+      play();
+      break;
+    case "gameOver" :
+      gameOver();
+      break;
+    default :
+      console.log("no match! check your mousePressed function")
+  }
 
 }
 
@@ -75,40 +74,37 @@ function play() {
   player1.y = mouseY;
   player1.display();
 
-  if(timer.isFinished())
-    gameState = "gameOver";  
-}
-
-
-if(dropTimer.isFinished()){
-  let p = new Box(random(width), -40);
-  presents.push(p); // add p to the array
-  dropTimer.start();
-  console.log(p);
-}
-
-for(let i =0; i < presents.length; i++){
-  presents[i].display();
-  presents[i].move();
-  presents[i].spin();
-  
-
-  if(presents[i].y > height){
-    presents.splice(i, 1); // remove from array
-    score --;
+  if(timer.isFinished()){
+    gameState = "gameOver";
+  }  
+  // all of this was supposed to be inside play(). Moved it for you -DW
+  if(dropTimer.isFinished()){
+    let p = new Box(random(width), -40);
+    presents.push(p); // add p to the array
+    dropTimer.start();
+    console.log(p);
   }
 
-  let d = dist(presents[i].x, presents[i].y, player1.x, player1.y);
-  if(d < 50){
-    presents.splice(i, 1);
-    score ++;
+  for(let i =0; i < presents.length; i++){
+    presents[i].display();
+    presents[i].move();
+    presents[i].spin();
+    
+    if(presents[i].y > height){
+      presents.splice(i, 1); // remove from array
+      score --;
+    }
+    let d = dist(presents[i].x, presents[i].y, player1.x, player1.y);
+    if(d < 50){
+      presents.splice(i, 1);
+      score ++;
+    }
   }
+  text("elapsed time: " + timer.elapsedTime, width/2, 100)
+  text("Score: ' + score, 20, 40");
 }
-text("elapsed time: " + timer.elapsedTime, width/2, 100)
-function gameOver() {
-  // this is what you see when the game ends
-text("Score: ' + score, 20, 40");
 
+function gameOver(){
   background(0);
   fill(255, 0, 0)
   textAlign(CENTER);
